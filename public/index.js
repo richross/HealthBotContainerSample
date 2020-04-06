30f114525a67e32faa00401b47f270e5a1de5a42
Rich Ross <rich.ross@live.com>
2020-04-06 16:12:56 -0400

    Update index.js
    triage this time

--- public/index.js
+++ public/index.js
@@ -119,7 +119,7 @@ function initBotConversation() {
 
                             // Use the following activity to proactively invoke a bot scenario
                             triggeredScenario: {
-                                trigger: "covid19_assessment"
+                                trigger: "triage"
                             }
                         }
                     }
