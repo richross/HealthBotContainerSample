50bcba1b96397a10ecdbafcdc61a40193c9e932e
Rich Ross <rich.ross@live.com>
2020-04-06 15:58:40 -0400

    Update index.js

--- public/index.js
+++ public/index.js
@@ -118,15 +118,9 @@ function initBotConversation() {
                             jsonWebToken: jsonWebToken,
 
                             // Use the following activity to proactively invoke a bot scenario
-                            /*
                             triggeredScenario: {
-                                trigger: "{scenario_id}",
-                                args: {
-                                    myVar1: "{custom_arg_1}",
-                                    myVar2: "{custom_arg_2}"
-                                }
+                                trigger: "covid19_assessment"
                             }
-                            */
                         }
                     }
                 }
